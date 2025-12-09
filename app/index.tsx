040fd7ae68d389708c6b58c2b2ab58fc5999b21c
Huỳnh Sang <huynhsang060305@gmail.com>
2025-12-09 18:29:21 +0700

    chore: create linking new screens and styling customize navigation links

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.view}>
+      <Text>bla bla bla</Text>
+      <Link href="/login" style={style.navButton}>
+        Login Page
+      </Link>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navButton: {
+    width: 100,
+    height: 20,
+    backgroundColor: "lightblue",
+    borderRadius: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+    marginTop: 20,
+  },
+});
