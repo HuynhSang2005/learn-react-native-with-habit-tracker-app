54718ad15e736077de86c91088a6657ef79875ac
Huỳnh Sang <huynhsang060305@gmail.com>
2025-12-23 23:46:54 +0700

    Add tab bar icons and active tint color

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,9 +1,21 @@
 import { Tabs } from "expo-router";
-
+import Ionicons from "@expo/vector-icons/Ionicons";
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <Ionicons name="home-outline" size={24} color="black" />
+            ) : (
+              <Ionicons name="home" size={24} color={color} />
+            );
+          },
+        }}
+      />
       <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   );
